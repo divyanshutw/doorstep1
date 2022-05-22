45fa55c154ff6d10909853f432e3678297b1ae2e
Alankrit Bhardwaj <48072177+alan478@users.noreply.github.com>
2022-05-22 16:16:08 +0530

    Settings fragment Complete

--- app/src/main/java/com/example/doorstep/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/doorstep/fragments/ProfileFragment.kt
@@ -1,43 +1,19 @@
 package com.example.doorstep.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.doorstep.databinding.FragmentProfileBinding
-import com.example.doorstep.viewModels.ProfileViewModel
 
-class ProfileFragment : Fragment() {
+import androidx.preference.PreferenceFragmentCompat
+import com.example.doorstep.R
 
-    private var _binding: FragmentProfileBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+class ProfileFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this)[ProfileViewModel::class.java]
 
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_preference, rootKey)
     }
 }
